d57c0c0eb35843caa685c10c00d64cf26e97ca80
Denis Selimovic <denis.selimovic@klika.ba>
2023-02-13 16:38:58 +0100

    Update command type

--- src/commands/types.rs
+++ src/commands/types.rs
@@ -2,5 +2,5 @@ use crate::errors::command::CommandError;
 use crate::protocol::types::Type;
 use crate::storage::Storage;
 
-pub type ComandResult = Result<Type, CommandError>;
-pub type Command = fn(&Type, &mut Storage) -> ComandResult;
+pub type ComandResult<'a> = Result<&'a Type, CommandError>;
+pub type Command = for<'a, 'b> fn(&'a [Type], &'b mut Storage) -> ComandResult<'b>;
