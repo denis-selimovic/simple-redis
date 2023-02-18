33b4c939ca8205df6d1576b4d2bc43f239be3b76
Denis Selimovic <denis.selimovic@klika.ba>
2023-02-18 13:02:10 +0100

    Refactor client code to use async features

--- src/client.rs
+++ src/client.rs
@@ -1,72 +1,44 @@
-use std::io::{stdin, Write, Read};
-use std::net::TcpStream;
+use async_std::prelude::*;
+use async_std::io;
+use async_std::net;
+use std::marker::Unpin;
 
 use crate::errors::client::ClientError;
-use crate::protocol::deserializer::deserialize;
 use crate::protocol::serializer::serialize;
 use crate::protocol::types::Type;
 
 
-pub struct Client {
-    port: u16,
+pub async fn send_commands(mut stream: net::TcpStream ) -> Result<(), ClientError> {
+    let mut command_line = io::BufReader::new(io::stdin()).lines();
+
+    while let Some(command) = command_line.next().await {
+        match command {
+            Err(_) => return Err(ClientError::InputError),
+            Ok(command) => {
+                match command.strip_suffix("\n") {
+                    None => continue,
+                    Some(buff) => send_command(&mut stream, buff).await?,
+                };
+            }
+        }
+    }
+
+    Ok(())
 }
 
+async fn send_command<S>(stream: &mut S, cmd: &str) -> Result<(), ClientError>
+where
+    S: async_std::io::Write + Unpin
+{
+    let cmd_args = cmd
+        .split_whitespace()
+        .map(|s| Type::BulkString(s.to_string()))
+        .collect();
+    let bytes = serialize(&Type::Array(cmd_args));
 
-impl Client {
-    pub fn new(port: u16) -> Self {
-        Client{ port: port }
+    if let Err(_) = stream.write_all(&bytes).await {
+        return Err(ClientError::CommandExecutionError);
     }
 
-    pub fn start(&mut self) -> Result<(), ClientError> {
-        let address = format!("127.0.0.1:{}", self.port);
-        let stream_res = TcpStream::connect(address);
-
-        if let Err(_) = stream_res {
-            return Err(ClientError::ConnectionError(self.port));
-        }
-
-        let mut stream = stream_res.ok().unwrap();
-
-        loop {
-            let command = self.read_command()?;
-            let bytes = serialize(&command);
-
-            let result = self.fetch_result(&mut stream, bytes)?;
-            let mut iter = result.into_iter();
-
-            match deserialize(&mut iter) {
-                Err(_) => return Err(ClientError::CommandExecutionError),
-                Ok(t) => println!("{}", t.to_string())
-            };
-        }
-    }
-
-    fn read_command(&self) -> Result<Type, ClientError> {
-        let mut buff = String::new();
-
-        match stdin().read_line(&mut buff) {
-            Err(_) => Err(ClientError::InputError),
-            Ok(_) => {
-                let res = match buff.strip_suffix("\n") {
-                    Some(buff) => buff.split_whitespace().map(|s| s.to_string()).collect(),
-                    None => vec![],
-                };
-
-                let cmd = res.iter().map(|s| Type::BulkString(s.to_string())).collect();
-                Ok(Type::Array(cmd))
-            },
-        }
-    }
-
-    fn fetch_result(&self, stream: &mut TcpStream, bytes: Vec<u8>) -> Result<Vec<u8>, ClientError>{
-        if let Err(_) = stream.write_all(&bytes) {
-            return Err(ClientError::CommandExecutionError);
-        }
-
-        let mut response = vec![];
-        match stream.read(&mut response) {
-            Err(_) => Err(ClientError::CommandExecutionError),
-            Ok(_) => Ok(response),
-        }
-    }
+    Ok(())
 }
