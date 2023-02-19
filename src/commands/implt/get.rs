47de567733181d2112719e7d44eae8e02ff71de6
Denis Selimovic <denis.selimovic@klika.ba>
2023-02-19 13:33:44 +0100

    Update commands to receive shared storage

--- src/commands/implt/get.rs
+++ src/commands/implt/get.rs
@@ -1,17 +1,19 @@
+use std::sync::{Arc, Mutex};
+
 use crate::commands::types::CommandResult;
 use crate::errors::command::CommandError;
 use crate::protocol::types::Type;
 use crate::storage::Storage;
 
 
-pub fn get<'a, 'b>(args: &'a [Type], storage: &'b mut Storage) -> CommandResult {
+pub fn get<'a>(args: &'a [Type], storage: Arc<Mutex<Storage>>) -> CommandResult {
     if args.len() != 1 {
         return Err(CommandError::WrongParamNumber("GET".to_string(), 1, args.len()));
     }
 
     match &args[0] {
         Type::BulkString(s) | Type::SimpleString(s) => {
-            let value = storage.read(s);
+            let value = storage.lock().unwrap().read(s);
             
             match value {
                 None => Ok(Type::Null),
--- src/commands/implt/mset.rs
+++ src/commands/implt/mset.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use crate::commands::implt::set::set;
 use crate::commands::types::CommandResult;
 use crate::errors::command::CommandError;
@@ -5,13 +7,13 @@ use crate::protocol::types::Type;
 use crate::storage::Storage;
 
 
-pub fn mset<'a, 'b>(args: &'a [Type], storage: &'b mut Storage) -> CommandResult {
+pub fn mset<'a>(args: &'a [Type], storage: Arc<Mutex<Storage>>) -> CommandResult {
     if args.len() % 2 != 0  || args.len() == 0 {
         return Err(CommandError::WrongVariableParamNumber("MSET".to_string(), 2, args.len()));
     }
 
     for i in (0..args.len()).step_by(2) {
-        set(&args[i..=i + 1], storage)?;
+        set(&args[i..=i + 1], storage.clone())?;
     }
 
     Ok(Type::Integer((args.len() / 2) as i64))
